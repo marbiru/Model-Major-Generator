9bf4a2dea1ebbf458ba5645afb59a967a6a2b71d
marbiru <code@uribram.com>
2016-01-05 18:39:53 +0700

    pegs lists into separate file
    put the peg lists into a separate file, and now import them into
    main.py, for cleanliness/neatness

--- pegs.py
+++ pegs.py
@@ -1,5 +1,3 @@
-print "\n Welcome to the Major System program. This gives you an image for any number \n"
-
 single_digit_peg_list = [
 "hose",
 "hat",
@@ -115,31 +113,3 @@ pegs_list = [
 "beef",
 "pipe",
 ]
-
-mnemonic = ""
-
-the_input = raw_input("Enter the number you'd like to memorise \n")
-
-while len(the_input) > 1:
-
-	next_number = int(the_input[0:2])
-	
-	next_image = pegs_list[next_number]
-	
-	the_input = the_input[2:]
-	
-	mnemonic += " " + next_image
-
-if len(the_input) == 1:
-
-	next_number = int(the_input[0:1])
-	
-	next_image = single_digit_peg_list[next_number]
-	
-	mnemonic += " " + next_image
-
-else:
-
-	pass
-
-print mnemonic
